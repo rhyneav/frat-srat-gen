46708b3e141c742db5c7c7955de3029bf4174d17
Rhyne <rhyneav@gmail.com>
2016-08-26 19:23:21 +0000

    add random letter function and center style

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 /*global $*/
 
+function randomNumberGenerator(max) {
+    var max = 23
+    var min = 0;
+    
+    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return randomNumber;
+}
+
 $.getJSON("https://spreadsheets.google.com/feeds/list/1RsBx38ctnaIZFcTBi6jpcNtB5C1QF_57hrVGYpdmf9c/od6/public/basic?alt=json", function(data){
-    var i = 0;
-    for (i in data.feed.entry) {
-        var letter = data.feed.entry[i]["title"]["$t"];
+    for (var i = 0; i < 3; i++) {
+        var j = randomNumberGenerator();
+        var letter = data.feed.entry[j]["title"]["$t"];
         $(".letters").append("<h2>" + letter + "</h2>");
     }
 });
