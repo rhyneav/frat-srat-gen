c3396ef216bf232fec550826cc82761f91ac1429
Rhyne <rhyneav@gmail.com>
2016-08-29 17:23:05 +0000

    Add animations and reroll function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*global $*/
 
-function randomNumberGenerator(max) {
-    var max = 23
+function randomNumberGenerator() {
+    var max = 23;
     var min = 0;
     
     var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,9 +10,34 @@ function randomNumberGenerator(max) {
 }
 
 $.getJSON("https://spreadsheets.google.com/feeds/list/1RsBx38ctnaIZFcTBi6jpcNtB5C1QF_57hrVGYpdmf9c/od6/public/basic?alt=json", function(data){
-    for (var i = 0; i < 3; i++) {
-        var j = randomNumberGenerator();
-        var letter = data.feed.entry[j]["title"]["$t"];
-        $(".letters").append("<h2>" + letter + "</h2>");
+    function goGreek () {
+        for (var i = 0; i < 3; i++) {
+            var j = randomNumberGenerator();
+            var letter = data.feed.entry[j]["title"]["$t"];
+            var letterName = letter.replace("&", "");
+            letterName = letterName.replace(";", "");
+            
+            console.log(data.feed.entry)
+            
+            $(".letters").append("<h2 class='animated hidden letter" + i + "'>" + letter + "</h2>");
+            if (i != 2) {
+                $(".description-text").append("<span class='text'>" + letterName + ", ");
+            } else {
+                $(".description-text").append(letterName);
+            }
+            
+            $(".letter" + i).delay(2000).removeClass("hidden").addClass("bounceIn");
+            
+        }
     }
+    goGreek()
+    
+    $("#reRoll").click(function() {
+        $("h2").remove();
+        $(".description-text").text("");
+        goGreek();
+    });
+
 });
+
+// Listeners
