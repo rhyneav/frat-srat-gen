282603462bae90ac8b46ab68d0de867130e57c4f
Rhyne <rhyneav@gmail.com>
2016-09-28 23:02:27 +0000

    Make length of JSON dynamic

--- script.js
+++ script.js
@@ -45,19 +45,21 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1RsBx38ctnaIZFcTBi6jpcNtB5
     
     $.getJSON("https://spreadsheets.google.com/feeds/list/1FCgjlBm1jL_Fvn0rlAgbwaVl0jv0aB67OEszwUVSNAo/od6/public/basic?alt=json", function(phrases) {
         function slogan() {
+            var jsonLength = phrases.feed.entry.length;
+
             // Get two adjectives
-            var j = randomNumberGenerator(0, 51);
+            var j = randomNumberGenerator(0, jsonLength - 1);
             var adj1 = phrases.feed.entry[j]["title"]["$t"];
 
-            var k = randomNumberGenerator(0, 51);
+            var k = randomNumberGenerator(0, jsonLength - 1);
             var adj2 = phrases.feed.entry[k]["title"]["$t"];
 
             // Get two nouns
-            var l = randomNumberGenerator(0, 51);
+            var l = randomNumberGenerator(0, jsonLength - 1);
             var noun1 = phrases.feed.entry[l]["content"]["$t"];
             noun1 = noun1.slice(6, noun1.length);
 
-            var m = randomNumberGenerator(0, 51);
+            var m = randomNumberGenerator(0, jsonLength - 1);
             var noun2 = phrases.feed.entry[m]["content"]["$t"];
             noun2 = noun2.slice(6, noun2.length);
 
@@ -77,7 +79,7 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1RsBx38ctnaIZFcTBi6jpcNtB5
             $(".container").css("background-color", randomColorGenerator());
         }
         
-        // Make start button not available until everything has been loaded
+        // Make start button not available until JSON has been loaded
         getStarted();
         
         // Listeners
